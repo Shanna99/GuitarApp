1031d5be167a8bc6419f21ac77b77c50a50eab16
Shanna <32540537+Shanna99@users.noreply.github.com>
2026-02-25 05:08:18 +0000

    change

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,28 +3,24 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
 
-// Enable Swagger in all environments (optional)
+
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "GuitarApp API V1");
-    c.RoutePrefix = "swagger"; // optional, default is "swagger"
 });
-
-// Middlewares
-app.UseHttpsRedirection();
-app.UseAuthorization();
-
-// Map controllers
-app.MapControllers();
-
-// Run app
-app.Run();
